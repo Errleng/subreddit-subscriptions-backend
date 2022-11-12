19eb53f2580eb6ed43a3f4579a7fea70153d152a
Daze <44749931+Errleng@users.noreply.github.com>
2022-11-11 20:36:33 -0500

    Config print

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 db.connectToMongo(() => {
     console.log('Connected to MongoDB');
-    console.log(config);
+    console.log('Config:', config);
     db.getDb()
         .collection(config.submissionsCollection)
         .createIndex(
